299d03cf2b25b4bd0ec91c0db3252f3c5688cf7e
thieu879 <147359883+thieu879@users.noreply.github.com>
2024-01-31 22:48:01 +0700

    Add files via upload

--- hackathon/hackathon.js
+++ hackathon/hackathon.js
@@ -1,88 +1,105 @@
 let heroInfo = {
-  name: "Minh Cường",
-  job: "freelance react developer",
-  imgUrl: "https://picsum.photos/200/300",
-  description:
-    "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi porro excepturi assumenda, culpa cupiditate, veritatis nobis rerum blanditiis necessitatibus dolorum amet sed voluptatum aspernatur impedit ducimus veniam dicta itaque ratione!",
-};
-localStorage.setItem("heroInfo", JSON.stringify(heroInfo));
-
-
-let recentProjects = [
-  {
-    id: 1,
-    imgUrl: "https://picsum.photos/300/300",
-    projectName: "Auto Drive Project",
-    link: "https://abcd-example.com",
-    tags: ["Angular", "React", "Jquery"],
-  },
-  {
-    id: 2,
-    imgUrl: "https://picsum.photos/300/300",
-    projectName: "Ecommerce Project",
-    link: "https://abcd-example.com",
-    tags: ["Bootstrap", "CSS", "Javascript"],
-  },
-  {
-    id: 3,
-    imgUrl: "https://picsum.photos/300/300",
-    projectName: "Chat Application",
-    link: "https://abcd-example.com",
-    tags: ["MongoDB", "Javascript"],
-  },
-  {
-    id: 4,
-    imgUrl: "https://picsum.photos/300/300",
-    projectName: "Social Media Platform",
-    link: "https://abcd-example.com",
-    tags: ["Vue", "Javascript"],
-  },
-  {
-    id: 5,
-    imgUrl: "https://picsum.photos/300/300",
-    projectName: "Image Sharing Platform",
-    link: "https://abcd-example.com",
-    tags: ["React", "Javascript"],
-  },
-];
-localStorage.setItem("recentProjects", JSON.stringify(recentProjects));
-
-
-
-
-let services = [
-  {
-    id: 1,
-    imgUrl: "https://picsum.photos/300/300",
-    serviceName: "Website Design",
-    description:
-      "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Qui veritatis tenetur corrupti obcaecati ipsam culpa et totam fuga eligendi",
-  },
-  {
-    id: 2,
-    imgUrl: "https://picsum.photos/300/300",
-    serviceName: "Figma Design",
-    description:
-      "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Qui veritatis tenetur corrupti obcaecati ipsam culpa et totam fuga eligendi",
-  },
-  {
-    id: 3,
-    imgUrl: "https://picsum.photos/300/300",
-    projectName: "SEO Marketing",
-    description:
-      "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Qui veritatis tenetur corrupti obcaecati ipsam culpa et totam fuga eligendi",
-  },
-  {
-    id: 4,
-    imgUrl: "https://picsum.photos/300/300",
-    projectName: "Graphic Design",
-    description:
-      "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Qui veritatis tenetur corrupti obcaecati ipsam culpa et totam fuga eligendi",
-  },
-];
-
-
-localStorage.setItem("services", JSON.stringify(services));
+    name: "Minh Cường",
+    job: "freelance react developer",
+    imgUrl: "https://picsum.photos/200/300",
+  };
+  
+  let personalDetail = {
+    name: "Minh Cường",
+    dob: "06 June 1995",
+    spokenLanguages: ["English", "France", "German"],
+    nationality: "Vietnam",
+    interest: ["Music", "Reading", "Journey"],
+    techs: [
+      {
+        id: 1,
+        imgUrl: "https://i.postimg.cc/cHdfNH2Z/android.png",
+        techName: "Android",
+        exp: 2,
+      },
+      {
+        id: 2,
+        imgUrl: "https://i.postimg.cc/nrCjHQk8/Angular.png",
+        techName: "Angular",
+        exp: 1,
+      },
+      {
+        id: 3,
+        imgUrl: "https://i.postimg.cc/G3VJ4csP/bootstrap.png",
+        techName: "Bootstrap",
+        exp: 3,
+      },
+      {
+        id: 4,
+        imgUrl: "https://i.postimg.cc/dt91z6v7/vue.png",
+        techName: "Vue",
+        exp: 2,
+      },
+      {
+        id: 5,
+        imgUrl: "https://i.postimg.cc/XNdXg3zk/react.png",
+        techName: "React",
+        exp: 2 / 3,
+      },
+      {
+        id: 6,
+        imgUrl: "https://i.postimg.cc/PfgYt2B2/mongodb.png",
+        techName: "Mongodb",
+        exp: 0.25,
+      },
+      {
+        id: 7,
+        imgUrl: "https://i.postimg.cc/RZzFYYjx/laravel.png",
+        techName: "Laravel",
+        exp: 1,
+      },
+      {
+        id: 8,
+        imgUrl: "https://i.postimg.cc/X7N3ybSJ/nodejs-icon.png",
+        techName: "Node.js",
+        exp: 5 / 6,
+      },
+    ],
+  };
+  
+  let projects = [
+    {
+      id: 1,
+      imgUrl: "https://i.postimg.cc/nrCjHQk8/Angular.png",
+      projectName: "Auto Drive Project",
+      link: "https://abcd-example.com",
+      tags: ["Angular", "React", "Jquery"],
+    },
+    {
+      id: 2,
+      imgUrl: "https://i.postimg.cc/G3VJ4csP/bootstrap.png",
+      projectName: "Ecommerce Project",
+      link: "https://abcd-example.com",
+      tags: ["Bootstrap", "CSS", "Javascript"],
+    },
+    {
+      id: 3,
+      imgUrl: "https://i.postimg.cc/PfgYt2B2/mongodb.png",
+      projectName: "Chat Application",
+      link: "https://abcd-example.com",
+      tags: ["MongoDB", "Javascript"],
+    },
+    {
+      id: 4,
+      imgUrl: "https://i.postimg.cc/dt91z6v7/vue.png",
+      projectName: "Social Media Platform",
+      link: "https://abcd-example.com",
+      tags: ["Vue", "Javascript"],
+    },
+    {
+      id: 5,
+      imgUrl: "https://i.postimg.cc/XNdXg3zk/react.png",
+      projectName: "Image Sharing Platform",
+      link: "https://abcd-example.com",
+      tags: ["React", "Javascript"],
+    },
+  ];
+  let project = localStorage.setItem("storage",JSON.stringify(projects));
 
 
 
